fc6a542fb32f77a953ab211b3e1ab2b32adf6e5d
Hage Woche <66566707+Hage714@users.noreply.github.com>
2024-07-18 21:23:27 +0300

    Netlify changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-                <a className="navbar-brand" href="/">Home</a>
+                <a className="navbar-brand" href="/">Index</a>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
